6cf636bbf8a55cb6d9e547f494bc876da1b9a6c9
Metages Firheta <metagesgashe@gmail.com>
2021-12-03 22:26:01 -0500

    game completed

--- rock-paper-scissors/src/Launcher.java
+++ rock-paper-scissors/src/Launcher.java
@@ -5,20 +5,49 @@ public class Launcher {
 		
 		Scanner scan = new Scanner(System.in);
 		
-		String player1 = scan.nextLine();
-		player1= player1.toLowerCase().trim();
+		boolean play = true;
+		while(play) {
+			System.out.println("Welcome to the rock-paper-scissors game");
+			System.out.println("please choose *rock*, *paper* or *scissors*");
+			
+			String player1 = scan.nextLine();
+			player1= player1.toLowerCase().trim();
+			System.out.println("player 1: " + player1);
+			
+			
+			String player2 = scan.nextLine();
+			player2 = player2.toLowerCase().trim();
+			System.out.println("player 2: " + player2);
+			System.out.println();
+			
+			System.out.println(rps(player1, player2));
+			
+			System.out.println();
+			System.out.println("Do you want to play more? (please say *yes* or *no*)");
+			String answer = scan.nextLine(); 
+			
+			answer = answer.toLowerCase().trim();
+			
+			if (answer.equals("yes")) {
+				play = true;
+			} else if (answer.equals("no")) {
+				System.out.println("Thank you for playing!");
+				play = false;
+			} else {
+				System.out.println("please say yes or no");
+				System.out.println("Thank you for playing!");
+				play = false;
+			}
+		}
 		
-		
-		String player2 = scan.nextLine();
-		player2 = player2.toLowerCase().trim();
-		
-		System.out.println(rps(player1, player2));
 		
 		
 		scan.close();
 	}
 	
 	public static String rps(String player1, String player2) {
+		
+		
 		if (player1.equals("rock") && player2.equals("paper")) {
 			return "player 2 wins";
 		} else if (player1.equals("rock") && player2.equals("scissors")) {
